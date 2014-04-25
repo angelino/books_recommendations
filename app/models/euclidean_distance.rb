a939cf32a97dfac67fc11d39804f72c56b0ff7c0
Lucas Angelino <lucas.angelino@gmail.com>
2014-04-25 01:25:52 -0300

    Fix problem with datatype of returned value

--- app/models/euclidean_distance.rb
+++ app/models/euclidean_distance.rb
@@ -1,19 +1,28 @@
 class EuclideanDistance
   def sim_distance(prefs, person1, person2)
+    #puts "Preferences: #{prefs}"
+    puts "Person1: #{person1}"
+    puts "Person2: #{person2}"
+
     shared_items = {}
 
     prefs[person1].each do |k, v|
       shared_items[k] = 1 if prefs[person2][k]
     end
 
-    #if they have no ratings in common, return 0
+    # if they have no ratings in common, return 0
+    puts "NO RATINGS IN COMMON to #{person1} and #{person2}" if shared_items.size == 0
     return 0 if shared_items.size == 0
 
     # Add up the squares of all the differences
     squares = []
     prefs[person1].each do |k, v|
-      if prefs[person2][k]
-        squares << ((prefs[person1][k] - prefs[person2][k]) ** 2)
+      v2 = prefs[person2][k]
+
+      if v2
+        square = ((v - v2) ** 2)
+        puts "(#{v} - #{v2}) ** 2 = #{square}"
+        squares << square 
       end
     end
 
@@ -22,6 +31,12 @@ class EuclideanDistance
       sum_of_squares += s
     end
 
-    return 1 / (1 + sum_of_squares)
+    #puts "Sum of Squares: #{sum_of_squares}"
+
+    distance =  1 / (1.0 + sum_of_squares)
+
+    puts "Euclidean Distance #{distance}"
+
+    distance
   end
 end
